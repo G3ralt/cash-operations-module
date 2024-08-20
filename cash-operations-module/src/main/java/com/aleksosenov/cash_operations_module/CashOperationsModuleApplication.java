90074fd2fe7a1d9608fc68694854d0439cada311
Aleksandar Osenov <aosenov@gmail.com>
2024-08-20 12:45:51 +0300

    Init

---
+++ cash-operations-module/src/main/java/com/aleksosenov/cash_operations_module/CashOperationsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.aleksosenov.cash_operations_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashOperationsModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashOperationsModuleApplication.class, args);
+	}
+
+}
